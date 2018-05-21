5c428a4e4b0f287343d77be9428b1f4b8a2cc494
Mikhail Shukshin <merqrylive@gmail.com>
2018-05-21 19:21:05 +0300

    new contract

--- src/contract/storage.js
+++ src/contract/storage.js
@@ -1,6 +1,6 @@
 const abi = require('./storage_abi.json');
 
 module.exports = {
-    address: '0xC092a31205494931aEb15B12aB91ed92af77F266',
+    address: '0x3990D27cEf1423555ae114faEe758fb8314Bf2e5',
     abi: abi
 }
